52e59e4a7c66166205d0913338c290d4ea459761
Твоето Име <@.com>
2026-03-09 19:28:34 +0200

    Fix: Correct imports in simulatorExercises

--- src/data/simulatorExercises.ts
+++ src/data/simulatorExercises.ts
@@ -1,7 +1,9 @@
 import { week1InternExercises } from './exercises/week1Intern';
 import { week2JuniorExercises } from './exercises/week2Junior';
+
 const allExercises = [
   ...week1InternExercises,
   ...week2JuniorExercises,
-  ...
-]
+];
+
+export default allExercises;
